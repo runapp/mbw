2b74df66470a25708d9123c4bc965f7999a75c3a
Ben Ma <runappz@gmail.com>
2020-12-06 22:32:06 +0800

    format

--- mbw.c
+++ mbw.c
@@ -12,7 +12,6 @@
 #include <sys/time.h>
 #include <time.h>
 #include <string.h>
-#include <unistd.h>
 
 /* how many runs to average by default */
 #define DEFAULT_NR_LOOPS 10
@@ -76,19 +75,21 @@ void usage()
 long *make_array(unsigned long long asize)
 {
     unsigned long long t;
-    unsigned int long_size=sizeof(long);
+    unsigned int long_size = sizeof(long);
     long *a;
 
-    a=calloc(asize, long_size);
+    a = calloc(asize, long_size);
 
-    if(NULL==a) {
+    if (NULL == a)
+    {
         perror("Error allocating memory");
         exit(1);
     }
 
     /* make sure both arrays are allocated, fill with pattern */
-    for(t=0; t<asize; t++) {
-        a[t]=0xaa;
+    for (t = 0; t < asize; t++)
+    {
+        a[t] = 0xaa;
     }
     return a;
 }
@@ -105,36 +106,44 @@ double worker(unsigned long long asize, long *a, long *b, int type, unsigned lon
     unsigned long long t;
     struct timeval starttime, endtime;
     double te;
-    unsigned int long_size=sizeof(long);
+    unsigned int long_size = sizeof(long);
     /* array size in bytes */
-    unsigned long long array_bytes=asize*long_size;
+    unsigned long long array_bytes = asize * long_size;
 
-    if(type==TEST_MEMCPY) { /* memcpy test */
+    if (type == TEST_MEMCPY)
+    { /* memcpy test */
         /* timer starts */
         gettimeofday(&starttime, NULL);
         memcpy(b, a, array_bytes);
         /* timer stops */
         gettimeofday(&endtime, NULL);
-    } else if(type==TEST_MCBLOCK) { /* memcpy block test */
-        char* aa = (char*)a;
-        char* bb = (char*)b;
+    }
+    else if (type == TEST_MCBLOCK)
+    { /* memcpy block test */
+        char *aa = (char *)a;
+        char *bb = (char *)b;
         gettimeofday(&starttime, NULL);
-        for (t=array_bytes; t >= block_size; t-=block_size, aa+=block_size){
-            bb=mempcpy(bb, aa, block_size);
+        for (t = array_bytes; t >= block_size; t -= block_size, aa += block_size)
+        {
+            bb = mempcpy(bb, aa, block_size);
         }
-        if(t) {
-            bb=mempcpy(bb, aa, t);
+        if (t)
+        {
+            bb = mempcpy(bb, aa, t);
         }
         gettimeofday(&endtime, NULL);
-    } else if(type==TEST_DUMB) { /* dumb test */
+    }
+    else if (type == TEST_DUMB)
+    { /* dumb test */
         gettimeofday(&starttime, NULL);
-        for(t=0; t<asize; t++) {
-            b[t]=a[t];
+        for (t = 0; t < asize; t++)
+        {
+            b[t] = a[t];
         }
         gettimeofday(&endtime, NULL);
     }
 
-    te=((double)(endtime.tv_sec*1000000-starttime.tv_sec*1000000+endtime.tv_usec-starttime.tv_usec))/1000000;
+    te = ((double)(endtime.tv_sec * 1000000 - starttime.tv_sec * 1000000 + endtime.tv_usec - starttime.tv_usec)) / 1000000;
 
     return te;
 }
@@ -150,20 +159,21 @@ double worker(unsigned long long asize, long *a, long *b, int type, unsigned lon
  */
 void printout(double te, double mt, int type)
 {
-    switch(type) {
-        case TEST_MEMCPY:
-            printf("Method: MEMCPY\t");
-            break;
-        case TEST_DUMB:
-            printf("Method: DUMB\t");
-            break;
-        case TEST_MCBLOCK:
-            printf("Method: MCBLOCK\t");
-            break;
+    switch (type)
+    {
+    case TEST_MEMCPY:
+        printf("Method: MEMCPY\t");
+        break;
+    case TEST_DUMB:
+        printf("Method: DUMB\t");
+        break;
+    case TEST_MCBLOCK:
+        printf("Method: MCBLOCK\t");
+        break;
     }
     printf("Elapsed: %.5f\t", te);
     printf("MiB: %.5f\t", mt);
-    printf("Copy: %.3f MiB/s\n", mt/te);
+    printf("Copy: %.3f MiB/s\n", mt / te);
     return;
 }
 
@@ -171,129 +181,147 @@ void printout(double te, double mt, int type)
 
 int main(int argc, char **argv)
 {
-    unsigned int long_size=0;
-    double te, te_sum; /* time elapsed */
-    unsigned long long asize=0; /* array size (elements in array) */
+    unsigned int long_size = 0;
+    double te, te_sum;            /* time elapsed */
+    unsigned long long asize = 0; /* array size (elements in array) */
     int i;
     long *a, *b; /* the two arrays to be copied from/to */
-    int o; /* getopt options */
+    int o;       /* getopt options */
     unsigned long testno;
 
     /* options */
 
     /* how many runs to average? */
-    int nr_loops=DEFAULT_NR_LOOPS;
+    int nr_loops = DEFAULT_NR_LOOPS;
     /* fixed memcpy block size for -t2 */
-    unsigned long long block_size=DEFAULT_BLOCK_SIZE;
+    unsigned long long block_size = DEFAULT_BLOCK_SIZE;
     /* show average, -a */
-    int showavg=1;
+    int showavg = 1;
     /* what tests to run (-t x) */
     int tests[MAX_TESTS];
-    double mt=0; /* MiBytes transferred == array size in MiB */
-    int quiet=0; /* suppress extra messages */
+    double mt = 0; /* MiBytes transferred == array size in MiB */
+    int quiet = 0; /* suppress extra messages */
 
-    tests[0]=0;
-    tests[1]=0;
-    tests[2]=0;
+    tests[0] = 0;
+    tests[1] = 0;
+    tests[2] = 0;
 
-    while((o=getopt(argc, argv, "haqn:t:b:")) != EOF) {
-        switch(o) {
-            case 'h':
-                usage();
+    while ((o = getopt(argc, argv, "haqn:t:b:")) != EOF)
+    {
+        switch (o)
+        {
+        case 'h':
+            usage();
+            exit(1);
+            break;
+        case 'a': /* suppress printing average */
+            showavg = 0;
+            break;
+        case 'n': /* no. loops */
+            nr_loops = strtoul(optarg, (char **)NULL, 10);
+            break;
+        case 't': /* test to run */
+            testno = strtoul(optarg, (char **)NULL, 10);
+            if (testno > MAX_TESTS - 1)
+            {
+                printf("Error: test number must be between 0 and %d\n", MAX_TESTS - 1);
                 exit(1);
-                break;
-            case 'a': /* suppress printing average */
-                showavg=0;
-                break;
-            case 'n': /* no. loops */
-                nr_loops=strtoul(optarg, (char **)NULL, 10);
-                break;
-            case 't': /* test to run */
-                testno=strtoul(optarg, (char **)NULL, 10);
-                if(testno>MAX_TESTS-1) {
-                    printf("Error: test number must be between 0 and %d\n", MAX_TESTS-1);
-                    exit(1);
-                }
-                tests[testno]=1;
-                break;
-            case 'b': /* block size in bytes*/
-                block_size=strtoull(optarg, (char **)NULL, 10);
-                if(0>=block_size) {
-                    printf("Error: what block size do you mean?\n");
-                    exit(1);
-                }
-                break;
-            case 'q': /* quiet */
-                quiet=1;
-                break;
-            default:
-                break;
+            }
+            tests[testno] = 1;
+            break;
+        case 'b': /* block size in bytes*/
+            block_size = strtoull(optarg, (char **)NULL, 10);
+            if (0 >= block_size)
+            {
+                printf("Error: what block size do you mean?\n");
+                exit(1);
+            }
+            break;
+        case 'q': /* quiet */
+            quiet = 1;
+            break;
+        default:
+            break;
         }
     }
 
     /* default is to run all tests if no specific tests were requested */
-    if( (tests[0]+tests[1]+tests[2]) == 0) {
-        tests[0]=1;
-        tests[1]=1;
-        tests[2]=1;
+    if ((tests[0] + tests[1] + tests[2]) == 0)
+    {
+        tests[0] = 1;
+        tests[1] = 1;
+        tests[2] = 1;
     }
 
-    if( nr_loops==0 && ((tests[0]+tests[1]+tests[2]) != 1) ) {
+    if (nr_loops == 0 && ((tests[0] + tests[1] + tests[2]) != 1))
+    {
         printf("Error: nr_loops can be zero if only one test selected!\n");
         exit(1);
     }
 
-    if(optind<argc) {
-        mt=strtoul(argv[optind++], (char **)NULL, 10);
-    } else {
+    if (optind < argc)
+    {
+        mt = strtoul(argv[optind++], (char **)NULL, 10);
+    }
+    else
+    {
         printf("Error: no array size given!\n");
         exit(1);
     }
 
-    if(0>=mt) {
+    if (0 >= mt)
+    {
         printf("Error: array size wrong!\n");
         exit(1);
     }
 
     /* ------------------------------------------------------ */
 
-    long_size=sizeof(long); /* the size of long on this platform */
-    asize=1024*1024/long_size*mt; /* how many longs then in one array? */
+    long_size = sizeof(long);             /* the size of long on this platform */
+    asize = 1024 * 1024 / long_size * mt; /* how many longs then in one array? */
 
-    if(asize*long_size < block_size) {
+    if (asize * long_size < block_size)
+    {
         printf("Error: array size larger than block size (%llu bytes)!\n", block_size);
         exit(1);
     }
 
-    if(!quiet) {
+    if (!quiet)
+    {
         printf("Long uses %d bytes. ", long_size);
-        printf("Allocating 2*%lld elements = %lld bytes of memory.\n", asize, 2*asize*long_size);
-        if(tests[2]) {
+        printf("Allocating 2*%lld elements = %lld bytes of memory.\n", asize, 2 * asize * long_size);
+        if (tests[2])
+        {
             printf("Using %lld bytes as blocks for memcpy block copy test.\n", block_size);
         }
     }
 
-    a=make_array(asize);
-    b=make_array(asize);
+    a = make_array(asize);
+    b = make_array(asize);
 
     /* ------------------------------------------------------ */
-    if(!quiet) {
+    if (!quiet)
+    {
         printf("Getting down to business... Doing %d runs per test.\n", nr_loops);
     }
 
     /* run all tests requested, the proper number of times */
-    for(testno=0; testno<MAX_TESTS; testno++) {
-        te_sum=0;
-        if(tests[testno]) {
-            for (i=0; nr_loops==0 || i<nr_loops; i++) {
-                te=worker(asize, a, b, testno, block_size);
-                te_sum+=te;
+    for (testno = 0; testno < MAX_TESTS; testno++)
+    {
+        te_sum = 0;
+        if (tests[testno])
+        {
+            for (i = 0; nr_loops == 0 || i < nr_loops; i++)
+            {
+                te = worker(asize, a, b, testno, block_size);
+                te_sum += te;
                 printf("%d\t", i);
                 printout(te, mt, testno);
             }
-            if(showavg) {
+            if (showavg)
+            {
                 printf("AVG\t");
-                printout(te_sum/nr_loops, mt, testno);
+                printout(te_sum / nr_loops, mt, testno);
             }
         }
     }
@@ -302,4 +330,3 @@ int main(int argc, char **argv)
     free(b);
     return 0;
 }
-
